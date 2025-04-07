8a2abb0bcbc8911c7e2b12e043d165872f81a6d8
Tyguy818 <tylerhancock818@yahoo.com>
2025-04-07 15:02:30 -0600

    Added Fragment Functionality

--- app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
+++ app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
@@ -1,20 +1,25 @@
 package edu.msudenver.cs3013.minfragment
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    fun startFragment(v : View) {
+        val frameLayout = findViewById<FragmentContainerView>(R.id.fragment_container)
+        val blueFragment = BlueFragment.newInstance("FIRST")
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, blueFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
